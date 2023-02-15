e7f60e931333243358a2f8d734f982aec2841480
rich-ter <me@philipposrichter.com>
2023-02-15 19:30:26 +0200

    making changes

--- front-end/fetchPost.js
+++ front-end/fetchPost.js
@@ -1,5 +1,3 @@
-const localhost = 'http://localhost:1337'
-
 function checkForArticle() {
     const urlParams = new URLSearchParams(window.location.search);
     const articleId = urlParams.get('id');
@@ -20,17 +18,10 @@ function getArticle(id) {
         document.getElementById("publish-date").innerHTML = data.data.attributes.publishedAt;
         document.getElementById("article-img").src = `http://localhost:1337${data.data.attributes.BlogImage.data.attributes.formats.medium.url}`
         document.getElementById("description").innerHTML = data.data.attributes.Description;
-    }).catch(function (err) {
+    })
+    .catch(function (err) {
         console.warn('Something went wrong.', err);
     })
-    .then(displayArticle)
-}
-    
-function displayArticle() {
-    document.getElementById("article-title").innerHTML = data.data.attributes.Title;
-    const showdown = window.showdown;
-    const converter = new showdown.Converter();
-    document.getElementById("article-content").innerHTML = converter.makeHtml(article.content);
 }
 
 checkForArticle();
