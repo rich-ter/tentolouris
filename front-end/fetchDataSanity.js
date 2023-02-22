d3bad423e43636eff93e73d81606f101c0669a50
rich-ter <me@philipposrichter.com>
2023-02-22 13:15:15 +0200

    making the url changes and converting to sanity

--- front-end/fetchDataSanity.js
+++ front-end/fetchDataSanity.js
@@ -3,19 +3,31 @@ function fetchData(){
 
     const PROJECT_ID = "lmk4epg5";
     const DATASET = "production";
-    
-    let QUERY = encodeURIComponent('*[_type == "post"]{title,body,mainImage}');
+    const CDN_URL = 'https://cdn.sanity.io/images/lmk4epg5/production/'
+
+    let QUERY = encodeURIComponent('*[_type == "post"]{_id,title,publishedAt,mainImage}');
     let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
+
+    function fixImageUrl(imageUrl){
+        removingThePrefix = imageUrl.replace('image-', '')
+        removingTheSuffix = removingThePrefix.replace('-jpg', '.jpg')
+        return removingTheSuffix
+    }
 
     fetch(URL)
         .then(response => {
         return response.json();
     })
     .then(data => {
+        console.log(data.result)
         const html = data.result.map (blog=> {
+            console.log(`the url of the ${blog.title} is: ${CDN_URL}${fixImageUrl(blog.mainImage.asset._ref)}`)
                 return `
                     <div class="card blog-post w-dyn-item">
+              <a href=${`./detail_post.html?id=${blog._id}`} class="blog-post-wrapper w-inline-block"><img src=${`${CDN_URL}${fixImageUrl(blog.mainImage.asset._ref)}`} alt="" class="blog-post-image">
+
                 <div class="card-content" >
+                  <div class="blog-post-date">${blog.publishedAt}</div>
                   <h3 class="blog-post-title">${blog.title}</h3>
                   <p id="api-date">Api excerpt</p>
                   <div class="blog-post-link-wrapper">
@@ -30,7 +42,6 @@ function fetchData(){
         document.querySelector('#api-render').innerHTML = ("afterbegin", html);
     })
 };
-
 
     
 fetchData();
--- front-end/fetchPost.js
+++ front-end/fetchPost.js
@@ -8,16 +8,48 @@ function checkForArticle() {
     }
 }
 
-function getArticle(id) {
-    const article_url = `http://localhost:1337/api/blog-posts/${id}?populate=BlogImage`
+// a function to return the description as a json string
+function toPlainText(blocks = []) {
+  return blocks
+    // loop through each block
+    .map(block => {
+      // if it's not a text block with children, 
+      // return nothing
+      if (block._type !== 'block' || !block.children) {
+        return ''
+      }
+      // loop through the children spans, and join the
+      // text strings
+      return block.children.map(child => child.text).join('')
+    })
+    // join the paragraphs leaving split by two linebreaks
+    .join('\n\n')
+}
 
-    fetch(article_url).then(function (response) {
+function fixImageUrl(imageUrl){
+    removingThePrefix = imageUrl.replace('image-', '')
+    removingTheSuffix = removingThePrefix.replace('-jpg', '.jpg')
+    return removingTheSuffix
+}
+
+function getArticle(id) {
+    const PROJECT_ID = "lmk4epg5";
+    const DATASET = "production";
+    const CDN_URL = 'https://cdn.sanity.io/images/zp7mbokg/production/'
+
+    let QUERY = encodeURIComponent(`*[_id == "${id}"]{title,mainImage}`);
+
+    let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
+
+
+    fetch(URL).then(function (response) {
         return response.json();
     }).then(function (data) {
-        document.getElementById("article-title").innerHTML = data.data.attributes.Title;
-        document.getElementById("publish-date").innerHTML = data.data.attributes.publishedAt;
-        document.getElementById("article-img").src = `http://localhost:1337${data.data.attributes.BlogImage.data.attributes.formats.medium.url}`
-        document.getElementById("description").innerHTML = data.data.attributes.Description;
+        console.log(data)
+        document.getElementById("article-title").innerHTML = data.result[0].title;
+        // document.getElementById("publish-date").innerHTML = data.data.attributes.publishedAt;
+        document.getElementById("article-img").src = `${CDN_URL}${fixImageUrl(blog.mainImage.asset._ref)}`
+        // document.getElementById("description").innerHTML = data.data.attributes.Description;
     })
     .catch(function (err) {
         console.warn('Something went wrong.', err);
